859e4c08271f3e395f0a8fc58ae34f1de74c7d3d
tayknight <will@willwyatt.com>
2012-03-17 20:54:01 -0500

    unignored credentials.js

---
+++ credentials.js
@@ -0,0 +1,7 @@
+var credentials = {
+  host: process.env['WILLCOUNT_HOST']
+  , user: process.env['WILLCOUNT_USER']
+  , password: process.env['WILLCOUNT_PASSWORD']
+  , database: process.env['WILLCOUNT_DATABASE']
+}
+module.exports = credentials;
